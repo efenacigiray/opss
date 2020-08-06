55e69e81896abc2dc041ddd05752b529f482839c
Efe Giray <efenacigiray@gmail.com>
2020-08-07 01:58:48 +0300

    deploy script update & admin location update

---
+++ control_panel/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opss.ctunca.com/admin/');
+define('HTTP_CATALOG', 'http://opss.ctunca.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opss.ctunca.com/admin/');
+define('HTTPS_CATALOG', 'http://opss.ctunca.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ctuncaco/opss.ctunca.com/admin/');
+define('DIR_SYSTEM', '/home/ctuncaco/opss.ctunca.com/system/');
+define('DIR_IMAGE', '/home/ctuncaco/opss.ctunca.com/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/home/ctuncaco/opss.ctunca.com/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_DATABASE', 'ctuncaco_opsstest');
+define('DB_USERNAME', 'ctuncaco_opssdemo');
+define('DB_PASSWORD', 'opss_demo*');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
