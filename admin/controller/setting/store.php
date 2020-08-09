febcec01262bb360df485a9fb159548b93ceb506
Efe Giray <efenacigiray@gmail.com>
2020-08-09 23:36:13 +0300

    fix recursion

--- admin/controller/setting/store.php
+++ admin/controller/setting/store.php
@@ -39,13 +39,13 @@ class ControllerSettingStore extends Controller {
 		$this->getForm();
 	}
 
-    public function recurse_copy($src, $dst) {
+    protected function recurse_copy($src, $dst) {
         $dir = opendir($src);
         @mkdir($dst);
         while(false !== ( $file = readdir($dir)) ) {
             if (( $file != '.' ) && ( $file != '..' )) {
                 if ( is_dir($src . '/' . $file) ) {
-                    recurse_copy($src . '/' . $file,$dst . '/' . $file);
+                    $this->recurse_copy($src . '/' . $file,$dst . '/' . $file);
                 }
                 else {
                     copy($src . '/' . $file,$dst . '/' . $file);
