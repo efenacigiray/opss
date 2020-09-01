c40b88e08bd7472561465a9c7711328683e78447
Efe Giray <efenacigiray@gmail.com>
2020-09-02 01:13:19 +0300

    yurtici settings

--- admin/controller/sale/kargo_etiket.php
+++ admin/controller/sale/kargo_etiket.php
@@ -1,120 +1,120 @@
 <?php
 class ControllerSaleKargoEtiket extends Controller {
-	private $error = array();
-	private $font = "FreeSansBold.ttf";
+    private $error = array();
+    private $font = "FreeSansBold.ttf";
 
-	public function index() {
+    public function index() {
 
-		$this->document->setTitle("Kargo Etiketleri");
+        $this->document->setTitle("Kargo Etiketleri");
 
-		$data['title'] = "Kargo Etiketleri";
+        $data['title'] = "Kargo Etiketleri";
 
-		if ($this->request->server['HTTPS']) {
-			$data['base'] = HTTPS_SERVER;
-		} else {
-			$data['base'] = HTTP_SERVER;
-		}
+        if ($this->request->server['HTTPS']) {
+            $data['base'] = HTTPS_SERVER;
+        } else {
+            $data['base'] = HTTP_SERVER;
+        }
 
-		$this->load->model('sale/order');
+        $this->load->model('sale/order');
 
-		$this->load->model('setting/setting');
+        $this->load->model('setting/setting');
 
-		$data['orders'] = array();
+        $data['orders'] = array();
 
-		$orders = array();
+        $orders = array();
 
-		if (isset($this->request->post['selected'])) {
-			$orders = $this->request->post['selected'];
-		} elseif (isset($this->request->get['order_id'])) {
-			$orders[] = $this->request->get['order_id'];
-		}
+        if (isset($this->request->post['selected'])) {
+            $orders = $this->request->post['selected'];
+        } elseif (isset($this->request->get['order_id'])) {
+            $orders[] = $this->request->get['order_id'];
+        }
 
-		foreach ($orders as $order_id) {
-			$order_info = $this->model_sale_order->getOrder($order_id);
+        foreach ($orders as $order_id) {
+            $order_info = $this->model_sale_order->getOrder($order_id);
 
-			// Make sure there is a shipping method
-			if ($order_info && $order_info['shipping_code']) {
-				$store_info = $this->model_setting_setting->getSetting('config', $order_info['store_id']);
+            // Make sure there is a shipping method
+            if ($order_info && $order_info['shipping_code']) {
+                $store_info = $this->model_setting_setting->getSetting('config', $order_info['store_id']);
 
-				if ($store_info) {
-					$store_address = $store_info['config_address'];
-					$store_email = $store_info['config_email'];
-					$store_telephone = $store_info['config_telephone'];
-					$store_fax = $store_info['config_fax'];
-				} else {
-					$store_address = $this->config->get('config_address');
-					$store_email = $this->config->get('config_email');
-					$store_telephone = $this->config->get('config_telephone');
-					$store_fax = $this->config->get('config_fax');
-				}
+                if ($store_info) {
+                    $store_address = $store_info['config_address'];
+                    $store_email = $store_info['config_email'];
+                    $store_telephone = $store_info['config_telephone'];
+                    $store_fax = $store_info['config_fax'];
+                } else {
+                    $store_address = $this->config->get('config_address');
+                    $store_email = $this->config->get('config_email');
+                    $store_telephone = $this->config->get('config_telephone');
+                    $store_fax = $this->config->get('config_fax');
+                }
 
-				$siparis_ontag = '2020';
+                $siparis_ontag = '2020';
 
-				$kargo_bak = $this->db->query("SELECT * FROM " . DB_PREFIX . "kargo_entegrasyon WHERE order_id = '" . (int) $order_id . "'");
-				$chechRecord = $kargo_bak->row;
+                $kargo_bak = $this->db->query("SELECT * FROM " . DB_PREFIX . "kargo_entegrasyon WHERE order_id = '" . (int) $order_id . "'");
+                $chechRecord = $kargo_bak->row;
 
-				if ($chechRecord) {
+                if ($chechRecord) {
 
-					if ($chechRecord['kargo_adi'] == 'aras') {
+                    if ($chechRecord['kargo_adi'] == 'aras') {
 
-						$barkod = $siparis_ontag.$order_id;
-						$kargo_adi = $chechRecord['kargo_adi'];
-						$musteri_no = '203027064';
-						$baslik = 'ARAS KARGO ETİKETİ';
+                        $barkod = $siparis_ontag.$order_id;
+                        $kargo_adi = $chechRecord['kargo_adi'];
+                        $musteri_no = '203027064';
+                        $baslik = 'ARAS KARGO ETİKETİ';
 
-					} elseif ($chechRecord['kargo_adi'] == 'ptt') {
+                    } elseif ($chechRecord['kargo_adi'] == 'ptt') {
 
-						$barkod = $chechRecord['barkod'];
-						$kargo_adi = $chechRecord['kargo_adi'];
-						$musteri_no = '203027064';
-						$baslik = 'PTT KARGO ETİKETİ';
+                        $barkod = $chechRecord['barkod'];
+                        $kargo_adi = $chechRecord['kargo_adi'];
+                        $musteri_no = '203027064';
+                        $baslik = 'PTT KARGO ETİKETİ';
 
-					}else {
+                    }else {
 
-						$barkod = $siparis_ontag.$order_id;
-						$kargo_adi = $chechRecord['kargo_adi'];
-						$musteri_no = '421439579';
-						$baslik = 'YURTİÇİ KARGO ETİKETİ';
+                        $barkod = $siparis_ontag.$order_id;
+                        $kargo_adi = $chechRecord['kargo_adi'];
+                        $musteri_no = $this->config->get('config_yt_code');
+                        $baslik = 'YURTİÇİ KARGO ETİKETİ';
 
-					}
+                    }
 
-				} else {
+                } else {
 
-					$barkod = false;
-					$kargo_adi = false;
-					$musteri_no = '-';
-					$baslik = '-';
-				}
+                    $barkod = false;
+                    $kargo_adi = false;
+                    $musteri_no = '-';
+                    $baslik = '-';
+                }
 
-				$data['orders'][] = array(
-					'order_id'	       => $order_id,
-					'barkod'	       => $barkod,
-					'kargo_adi'		   => $kargo_adi,
-					'baslik'		   => $baslik,
-					'musteri_no'	   => $musteri_no,
-					'store_name'       => $order_info['store_name'],
-					'store_address'    => nl2br($store_address),
-					'store_email'      => $store_email,
-					'store_telephone'  => $store_telephone,
-					'store_fax'        => $store_fax,
-					'email'            => $order_info['email'],
-					'telephone'        => $order_info['telephone'],
-					'firstname' 		=> $order_info['shipping_firstname'],
-					'lastname'  		=> $order_info['shipping_lastname'],
-					'address_1' 		=> $order_info['shipping_address_1'],
-					'address_2' 		=> $order_info['shipping_address_2'],
-					'city'      		=> $order_info['shipping_city'],
-					'zone'      		=> $order_info['shipping_zone'],
-					'shipping_method'  => $order_info['shipping_method'],
-					'total'            => $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value']),
+                $data['orders'][] = array(
+                    'order_id'         => $order_id,
+                    'barkod'           => $barkod,
+                    'kargo_adi'        => $kargo_adi,
+                    'baslik'           => $baslik,
+                    'musteri_no'       => $musteri_no,
+                    'store_name'       => $order_info['store_name'],
+                    'store_address'    => nl2br($store_address),
+                    'store_email'      => $store_email,
+                    'store_telephone'  => $store_telephone,
+                    'store_fax'        => $store_fax,
+                    'email'            => $order_info['email'],
+                    'telephone'        => $order_info['telephone'],
+                    'firstname'         => $order_info['shipping_firstname'],
+                    'lastname'          => $order_info['shipping_lastname'],
+                    'address_1'         => $order_info['shipping_address_1'],
+                    'address_2'         => $order_info['shipping_address_2'],
+                    'city'              => $order_info['shipping_city'],
+                    'zone'              => $order_info['shipping_zone'],
+                    'shipping_method'  => $order_info['shipping_method'],
+                    'total'            => $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value']),
                     'payment_method'   => $order_info['payment_method'],
                     'payment_code'     => $order_info['payment_code'],
-					'comment'          => nl2br($order_info['comment'])
-				);
-			}
-		}
+                    'comment'          => nl2br($order_info['comment'])
+                );
+            }
+        }
         $this->config->set('template_engine', 'template');
-		$this->response->setOutput($this->load->view('sale/kargo_etiket', $data));
-	}
+        $this->response->setOutput($this->load->view('sale/kargo_etiket', $data));
+    }
 
 }
